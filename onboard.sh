6811e087c00448175523383bc20b2df06db3d6b1
Richard C.N <111741533+iamRichardCN@users.noreply.github.com>
2022-11-22 10:02:03 +0100

    Update onboard.sh

--- onboard.sh
+++ onboard.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Shell Scripting.
 # This script will read a CSV file that contains 20 new Linux users.
 # This script will create each user on the server and add to an existing group called 'Developers'.
@@ -5,7 +7,7 @@
 # The user that is being created also must also have a default home folder
 # Each user should have a .ssh folder within its HOME folder. If it does not exist, then it will be created.
 # For each user’s SSH configuration, We will create an authorized_keys file and add the below public key.
-#!/bin/bash
+
 
 userfile=$(cat names.csv)
 PASSWORD=password
